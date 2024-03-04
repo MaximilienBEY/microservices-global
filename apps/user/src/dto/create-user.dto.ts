ebe06b4c9f51919eed0e4dbb28bfeeb2d1e92182
Maximilien Bey <bey.maximilien@gmail.com>
2024-03-04 17:42:03 +0100

    add user submodule

--- apps/user/src/dto/create-user.dto.ts
+++
@@ -1,4 +0,0 @@
-import { userCreateSchema } from "@app/common/schemas/user/schema"
-import { createZodDto } from "nestjs-zod"
-
-export class CreateUserDto extends createZodDto(userCreateSchema) {}
--- apps/user/src/dto/update-user.dto.ts
+++
@@ -1,4 +0,0 @@
-import { userUpdateSchema } from "@app/common/schemas/user/schema"
-import { createZodDto } from "nestjs-zod"
-
-export class UpdateUserDto extends createZodDto(userUpdateSchema) {}
