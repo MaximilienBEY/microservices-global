636be5d2f5d4cc8c0016acc35ddbe1785b472c0a
Maximilien Bey <bey.maximilien@gmail.com>
2024-03-07 17:56:46 +0100

    Update subproject commits and add configuration validation

--- libs/common/src/module.ts
+++ libs/common/src/module.ts
@@ -1,13 +1,29 @@
 import { Module } from "@nestjs/common"
+import { ConfigModule } from "@nestjs/config"
 import { APP_PIPE } from "@nestjs/core"
+import * as joi from "joi"
 import { ZodValidationPipe } from "nestjs-zod"
 
 import { AppAuthModule } from "./auth/auth.module"
 import { PrismaModule } from "./prisma/prisma.module"
 import { AppThrottlerModule } from "./throttler/throttler.module"
 
+const isCI = process.env.CI === "true"
+
 @Module({
-  imports: [PrismaModule, AppAuthModule, AppThrottlerModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: joi.object({
+        DATABASE_URL: isCI ? joi.string().optional() : joi.string().required(),
+        RABBIT_MQ_URL: isCI ? joi.string().optional() : joi.string().required(),
+        JWT_SECRET: isCI ? joi.string().optional() : joi.string().required(),
+      }),
+    }),
+    PrismaModule,
+    AppAuthModule,
+    AppThrottlerModule,
+  ],
   providers: [{ provide: APP_PIPE, useClass: ZodValidationPipe }],
   exports: [PrismaModule],
 })
